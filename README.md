7798bf7c4149438f50f1657861e0345353a88615
Francisco Aparicio Nascimento Coelho <faparicionc@gmail.com>
2026-03-09 10:30:01 -0300

    Organizando cards

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ProgramacaoWeb-(Avancado)-IFMG
+
 Curso de Programação Web pelo IFMG.
+
+Informações gerai:
+
+- gap: 8;
+- margin-top: 8%;
--- SEMANA1/script.js
+++
@@ -1,5 +0,0 @@
-function mostrarMensagem() {
-  let elemento = document.getElementById("mensagem");
-
-  elemento.innerHTML = "Você clicou no botão! 🚀";
-}
